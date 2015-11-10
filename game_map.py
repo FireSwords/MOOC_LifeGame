bf02461abbf6f3db841d87cb49d0f743a2eeda11
FireSword <sm_xj@163.com>
2015-11-10 15:46:37 +0800

    myself first

--- game_map.py
+++ game_map.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# @author XiaoTu
+# @author FireSword
 
 import random
 import sys
@@ -20,15 +20,16 @@ class GameMap(object):
     
     MAX_MAP_SIZE = 100
     MAX_CELL_VALUE = 1
-    MIN_CELL_VALUE = 0
+    MIN_CELL_VALUE = -1
     DIRECTIONS = (
+        (0, 2, ),
         (0, 1, ),
-        (1, -1, ),
-        (-1, 0, ),
-        (1, -1, ),
-        (-1, -1, ),
-        (-1, 0, ),
-        (-1, -1, ),
+        (2, 0, ),
+        (1, 0, ),
+        (0, -2, ),
+        (0, -1, ),
+        (-2, 0, ),
+		(-1, 0, ),
     )
 
     def __init__(self, rows, cols):
@@ -57,7 +58,10 @@ class GameMap(object):
         """
         for row in self.cells:
             for col_num in range(self.cols):
-                row[col_num] = 1 if random.random() < possibility_live else 0
+				if random().random()<possibility_wall:
+					row[col_num] = -1
+				else:
+					row[col_num] = 1 if random.random() < possibility_live else 0
 
     def set(self, row, col, val):
         """Set specific cell in the map."""
@@ -79,11 +83,12 @@ class GameMap(object):
         for d in self.DIRECTIONS:
             d_row = row + d[0]
             d_col = col + d[1]
-            if d_row >= self.rows:
-                d_row -= self.rows
-            if d_col >= self.cols:
-                d_col -= self.cols
-            count += self.cells[d_col][d_row]
+            if d_row >= self.rows or d_row<0:
+                continue
+            if d_col >= self.cols or d_col<0:
+                continue
+            if self.cells[d_col][d_row]==1:
+				count += 1
         return count
 
     def get_neighbor_count_map(self):
